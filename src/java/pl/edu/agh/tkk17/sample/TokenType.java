4e6c29b2329f9ff059c053e69302ab3bff9968b7
olagit <olagit@users.noreply.github.com>
2017-09-15 01:03:23 +0200

    Add files via upload

---
+++ src/java/pl/edu/agh/tkk17/sample/TokenType.java
@@ -0,0 +1,25 @@
+package pl.edu.agh.tkk17.sample;
+
+public enum TokenType
+{
+    END("END"),
+    NUM("NUM"),
+    ADD("ADD"),
+    SUB("SUB"),
+    MUL("MUL"),
+    DIV("DIV"),
+    LBR("LBR"),
+    RBR("RBR");
+
+    private final String name;
+
+    TokenType(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+}
